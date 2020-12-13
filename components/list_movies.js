6c3eff7e67bef943c2140c5768ce982c5937565a
Ebin Paul <ebinpaul@outlook.com>
2020-12-13 13:34:06 +0530

    Update list_movies.js

--- components/list_movies.js
+++ components/list_movies.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import { API } from '../api_service'
 
 function MovieList() {
+  
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    API.getMovies('77903b7a33ad265d3d2cc903e74e423bb19eae22')
+    .then(resp => setMovies(resp))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>List of Movies</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        data={movies}
+        renderItem={({item}) => (
+            <View>
+              <Text key={item.id}>{item.title}</Text>
+            </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -14,7 +32,7 @@ function MovieList() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffa',
     alignItems: 'center',
     justifyContent: 'center',
   },
