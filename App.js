0d570707cf18c1b452436e058dfd5be0d2aea27b
Ebin Paul <ebinpaul@outlook.com>
2020-12-13 13:34:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { MovieList } from './components/list_movies'
 
 export default function App() {
-
-  const [movies, setMovies] = useState([]);
 
   return (
     <MovieList />
