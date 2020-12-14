d1da538353757a071d9d9563c39cf134df78cbbc
Ebin Paul <ebinpaul@outlook.com>
2020-12-14 23:33:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
 import { MovieList } from './components/list_movies'
+import { MovieDetail } from './components/movie_detail'
 
-export default function App() {
+const AppNavigator = createStackNavigator({
+  List: {screen: MovieList},
+  Detail: {screen: MovieDetail}
+})
 
-  return (
-    <MovieList />
-  );
-}
+const App = createAppContainer(AppNavigator);
+
+export default App;
