56317111cf1e81a86a0736fcac1536e0c1324632
Ebin Paul <ebinpaul@outlook.com>
2020-12-16 13:06:15 +0530

    Update api_service.js

--- api_service.js
+++ api_service.js
@@ -13,4 +13,18 @@ export class API{
         ).then(resp => resp.json())
     }
 
+    static editMovie(token, movie_id, body){
+        return fetch(
+            `http://192.168.0.106:8000/api/movies/${movie_id}/`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Token ${token}`
+                },
+                body: JSON.stringify(body)
+            }
+        ).then(resp => resp.json())
+    }
+
 }
