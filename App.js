581df46842ea102e1d1dc978169f3a8ad20b1579
Ebin Paul <ebinpaul@outlook.com>
2020-12-17 17:48:14 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
-import { MovieList } from './components/list_movies'
-import { MovieDetail } from './components/movie_detail'
-import { EditMovie } from './components/edit_movie'
+import { UserAuthentication } from './components/user_auth';
+import { MovieList } from './components/list_movies';
+import { MovieDetail } from './components/movie_detail';
+import { EditMovie } from './components/edit_movie';
 
 const AppNavigator = createStackNavigator({
+  Authentication: {screen: UserAuthentication},
   List: {screen: MovieList},
   Detail: {screen: MovieDetail},
   Edit: {screen: EditMovie}
