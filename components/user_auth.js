0b736184d4043974383adb4019a1f00161c3aac2
Ebin Paul <ebinpaul@outlook.com>
2020-12-18 10:16:23 +0530

    Update user_auth.js

--- components/user_auth.js
+++ components/user_auth.js
@@ -3,21 +3,27 @@ import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { API } from '../api_service'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 function UserAuthentication(props) {  
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [registerView, setRegisterView] = useState(false);
 
     useEffect(()=>{
         getToken();
     }, [])
 
-    // editing the movie, saving the changes and bringing back the changes to the previous screen to display
     const userLogin = () => {
         API.userLogin({ username, password })
             .then(resp => saveToken(resp.token))
-        // props.navigation.navigate('List')
+        props.navigation.navigate('List')
+    }
+
+    const registerUser = () => {
+        API.userRegister({ username, password })
+            .then(resp => setRegisterView(false))
     }
 
     const saveToken = async (token) =>{
@@ -29,6 +35,10 @@ function UserAuthentication(props) {
         if(token){ 
             props.navigation.navigate('List')
         }
+    }
+
+    const toggleView = () => {
+        setRegisterView(!registerView);
     }
     
     return (
@@ -51,8 +61,11 @@ function UserAuthentication(props) {
                 autoCapitalize={"none"}
             />
 
-            <Button onPress={() => userLogin()} title="Login"/> 
-        
+            <Button onPress={() => {registerView ? registerUser() :userLogin()}} title={registerView ? "Register" : "Login"}/> 
+            <TouchableOpacity onPress={() => toggleView()}>
+                {registerView ? <Text style={styles.labelFooter2}>Already registered? Login here.</Text> 
+                : <Text style={styles.labelFooter1}>Don't have an account? Register here.</Text>}
+            </TouchableOpacity>       
         </View>
     );
 }
@@ -83,6 +96,20 @@ const styles = StyleSheet.create({
       margin: 10,
       backgroundColor: 'white',
   },
+  labelFooter1: {
+    fontSize: 15,
+    color: 'white',
+    paddingTop: 20,
+    paddingLeft: 85,
+    paddingRight: 10,
+},
+labelFooter2: {
+    fontSize: 15,
+    color: 'white',
+    paddingTop: 20,
+    paddingLeft: 105,
+    paddingRight: 10,
+},
 });
 
 export { UserAuthentication };
